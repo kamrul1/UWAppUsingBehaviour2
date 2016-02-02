4b8e0cb8a8611cab997405872e521a2baebb2f68
kamrul i <to.kamrul@outlook.com>
2016-02-02 21:22:10 +0000

    This example has a button click

---
+++ UWAppCustomBehaviorHandsOn/ShowDialogBehavior.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xaml.Interactivity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace UWAppCustomBehaviorHandsOn
+{
+    public class ShowDialogBehavior : DependencyObject, IBehavior
+    {
+        public string Message { get; set; }
+        public string MsgTitle { get; set; }
+
+        public DependencyObject AssociatedObject{ get; set;}
+
+
+        public void Attach(DependencyObject associatedObject)
+        {
+            this.AssociatedObject = associatedObject;
+            (this.AssociatedObject as Button).Click += Button_Click;
+        }
+
+
+
+        public void Detach()
+        {
+            (this.AssociatedObject as Button).Click -= Button_Click;
+        }
+
+
+
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            await new MessageDialog(this.Message, this.MsgTitle).ShowAsync();
+        }
+    }
+}
